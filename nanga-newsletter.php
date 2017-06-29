3494d06161d5963a90fabfc6a04bee3e4353a8e4
Panos Paganis <mallinanga@gmail.com>
2017-06-29 21:23:34 +0300

    WIP

--- nanga-newsletter.php
+++ nanga-newsletter.php
@@ -1,14 +1,14 @@
 <?php
-/*
-Author URI: https://www.vgwebthings.com/
-Author: VG web things
-Description: Simple Newsletter Form.
-Domain Path: /languages/
-Plugin Name: VG web things Newsletter Form
-Text Domain: nanga-newsletter
-Version: 1.0.0
-*/
-if ( ! defined('WPINC')) {
-    die;
-}
+/**
+ * Author URI: https://github.com/Mallinanga/
+ * Author: Panos Paganis
+ * Description: Simple Newsletter Form.
+ * Domain Path: /languages/
+ * Plugin Name: VG web things Newsletter Form
+ * Text Domain: nanga-newsletter
+ * Version: 1.0.0
+ */
+
+defined('WPINC') || die;
+
 require_once 'vendor/autoload.php';
